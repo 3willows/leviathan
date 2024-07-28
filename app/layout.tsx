c605d8bb03599b5e997fe519b0f929b161d6f214
Jasper Wong <wlcjasper@gmail.com>
2024-07-28 23:26:00 +0100

    Fix syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
-import { Inter } from 'next/font/google'
-import { cn } from '@/lib/utils'
-import './globals.css'
+import { Inter } from "next/font/google"
+import { cn } from "@/lib/utils"
+import "./globals.css"
 
 const fontHeading = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
 })
 
 const fontBody = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-body',
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
 })
 
-export default function Layout({ children : any }) {
+export default function Layout({ children }: { children: any }) {
   return (
     <html lang="en">
-      <body 
+      <body
         className={cn(
-          'antialiased m-8 custom-line-spacing leading-relaxed text-xl ',
+          "antialiased m-8 custom-line-spacing leading-relaxed text-xl ",
           fontHeading.variable,
           fontBody.variable
         )}
