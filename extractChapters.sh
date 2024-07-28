e5b00ade76ccaf9dc0749524d9b46d9cc989cc7f
Jasper Wong <wlcjasper@gmail.com>
2024-07-28 21:25:22 +0100

    Manually tweak the bash script

--- extractChapters.sh
+++ extractChapters.sh
@@ -4,7 +4,7 @@
 input_file="leviathan.html"
 
 # Counter for naming output files
-counter=1
+counter=0
 
 echo "Starting to process file: $input_file"
 
@@ -13,13 +13,14 @@ sed -n '/<div class="chapter">/,/<!--end chapter-->/p' "$input_file" |
 while IFS= read -r line
 do
     if [[ $line == '<div class="chapter">'* ]]; then
+        ((counter++))
+        echo "Incremented counter to: $counter"      
         # Start of a new chapter, create a new file
         output_file="chapter_$counter.txt"
     elif [[ $line == *'<!--end chapter-->' ]]; then
         # End of the chapter, append the end tag and increment counter
         echo "$line" >> "$output_file"
-        ((counter++))
-        echo "Incremented counter to: $counter"
+
     else
         echo "$line" >> "$output_file"
     fi
