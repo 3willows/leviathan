af9d1cfd608baac49fbbf372ba17d724957236e0
Jasper Wong <wlcjasper@gmail.com>
2024-07-29 00:11:07 +0100

    Fix apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@
 export default function Page() {
   return (
     <div className="bg-[#f5f5f5] text-[#333] font-serif mx-auto max-w-4xl px-8 py-12 shadow-lg">
-      <h1 className="text-4xl font-bold mb-8">Thomas Hobbes`&apos;`s Leviathan</h1>
+      <h1 className="text-4xl font-bold mb-8">Thomas Hobbes &rsquo;s Leviathan</h1>
       <h2 className="text-4xl font-bold mb-8">Table of Contents</h2>
       <ol className="list-decimal list-inside space-y-6">
         <li>
