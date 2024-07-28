0feee6472fb43ef1349dc2baa5149baa134abca2
Jasper Wong <wlcjasper@gmail.com>
2024-07-28 22:33:50 +0100

    Adding the chapters.

---
+++ app/chapter/16/page.tsx
@@ -0,0 +1,220 @@
+export default function Page() {return (<div>
+<h2><a id="link2HCH0016"></a>
+      CHAPTER XVI.<br></br>OF PERSONS, AUTHORS, AND THINGS PERSONATED
+    </h2>
+<p>
+      A Person What
+    </p>
+<p>
+      A PERSON, is he “whose words or actions are considered, either as his own,
+      or as representing the words or actions of an other man, or of any other
+      thing to whom they are attributed, whether Truly or by Fiction.”
+    </p>
+<h3><a id="link2H_4_0179"></a>
+      Person Naturall, And Artificiall
+    </h3>
+<p>
+      When they are considered as his owne, then is he called a Naturall Person:
+      And when they are considered as representing the words and actions of an
+      other, then is he a Feigned or Artificiall person.
+    </p>
+<h3><a id="link2H_4_0180"></a>
+      The Word Person, Whence
+    </h3>
+<p>
+      The word Person is latine: instead whereof the Greeks have Prosopon, which
+      signifies the Face, as Persona in latine signifies the Disguise, or
+      Outward Appearance of a man, counterfeited on the Stage; and somtimes more
+      particularly that part of it, which disguiseth the face, as a Mask or
+      Visard: And from the Stage, hath been translated to any Representer of
+      speech and action, as well in Tribunalls, as Theaters. So that a Person,
+      is the same that an Actor is, both on the Stage and in common
+      Conversation; and to Personate, is to Act, or Represent himselfe, or an
+      other; and he that acteth another, is said to beare his Person, or act in
+      his name; (in which sence Cicero useth it where he saies, “Unus Sustineo
+      Tres Personas; Mei, Adversarii, &amp; Judicis, I beare three Persons; my
+      own, my Adversaries, and the Judges;”) and is called in diverse occasions,
+      diversly; as a Representer, or Representative, a Lieutenant, a Vicar, an
+      Attorney, a Deputy, a Procurator, an Actor, and the like.
+    </p>
+<h3><a id="link2H_4_0181"></a>
+      Actor, Author; Authority
+    </h3>
+<p>
+      Of Persons Artificiall, some have their words and actions Owned by those
+      whom they represent. And then the Person is the Actor; and he that owneth
+      his words and actions, is the AUTHOR: In which case the Actor acteth by
+      Authority. For that which in speaking of goods and possessions, is called
+      an Owner, and in latine Dominus, in Greeke Kurios; speaking of Actions, is
+      called Author. And as the Right of possession, is called Dominion; so the
+      Right of doing any Action, is called AUTHORITY. So that by Authority, is
+      alwayes understood a Right of doing any act: and Done By Authority, done
+      by Commission, or Licence from him whose right it is.
+    </p>
+<h3><a id="link2H_4_0182"></a>
+      Covenants By Authority, Bind The Author
+    </h3>
+<p>
+      From hence it followeth, that when the Actor maketh a Covenant by
+      Authority, he bindeth thereby the Author, no lesse than if he had made it
+      himselfe; and no lesse subjecteth him to all the consequences of the same.
+      And therfore all that hath been said formerly, (Chap. 14) of the nature of
+      Covenants between man and man in their naturall capacity, is true also
+      when they are made by their Actors, Representers, or Procurators, that
+      have authority from them, so far-forth as is in their Commission, but no
+      farther.
+    </p>
+<p>
+      And therefore he that maketh a Covenant with the Actor, or Representer,
+      not knowing the Authority he hath, doth it at his own perill. For no man
+      is obliged by a Covenant, whereof he is not Author; nor consequently by a
+      Covenant made against, or beside the Authority he gave.
+    </p>
+<h3><a id="link2H_4_0183"></a>
+      But Not The Actor
+    </h3>
+<p>
+      When the Actor doth any thing against the Law of Nature by command of the
+      Author, if he be obliged by former Covenant to obey him, not he, but the
+      Author breaketh the Law of Nature: for though the Action be against the
+      Law of Nature; yet it is not his: but contrarily; to refuse to do it, is
+      against the Law of Nature, that forbiddeth breach of Covenant.
+    </p>
+<h3><a id="link2H_4_0184"></a>
+      The Authority Is To Be Shewne
+    </h3>
+<p>
+      And he that maketh a Covenant with the Author, by mediation of the Actor,
+      not knowing what Authority he hath, but onely takes his word; in case such
+      Authority be not made manifest unto him upon demand, is no longer obliged:
+      For the Covenant made with the Author, is not valid, without his
+      Counter-assurance. But if he that so Covenanteth, knew before hand he was
+      to expect no other assurance, than the Actors word; then is the Covenant
+      valid; because the Actor in this case maketh himselfe the Author. And
+      therefore, as when the Authority is evident, the Covenant obligeth the
+      Author, not the Actor; so when the Authority is feigned, it obligeth the
+      Actor onely; there being no Author but himselfe.
+    </p>
+<h3><a id="link2H_4_0185"></a>
+      Things Personated, Inanimate
+    </h3>
+<p>
+      There are few things, that are uncapable of being represented by Fiction.
+      Inanimate things, as a Church, an Hospital, a Bridge, may be Personated by
+      a Rector, Master, or Overseer. But things Inanimate, cannot be Authors,
+      nor therefore give Authority to their Actors: Yet the Actors may have
+      Authority to procure their maintenance, given them by those that are
+      Owners, or Governours of those things. And therefore, such things cannot
+      be Personated, before there be some state of Civill Government.
+    </p>
+<h3><a id="link2H_4_0186"></a>
+      Irrational
+    </h3>
+<p>
+      Likewise Children, Fooles, and Mad-men that have no use of Reason, may be
+      Personated by Guardians, or Curators; but can be no Authors (during that
+      time) of any action done by them, longer then (when they shall recover the
+      use of Reason) they shall judge the same reasonable. Yet during the Folly,
+      he that hath right of governing them, may give Authority to the Guardian.
+      But this again has no place but in a State Civill, because before such
+      estate, there is no Dominion of Persons.
+    </p>
+<h3><a id="link2H_4_0187"></a>
+      False Gods
+    </h3>
+<p>
+      An Idol, or meer Figment of the brain, may be Personated; as were the Gods
+      of the Heathen; which by such Officers as the State appointed, were
+      Personated, and held Possessions, and other Goods, and Rights, which men
+      from time to time dedicated, and consecrated unto them. But idols cannot
+      be Authors: for a Idol is nothing. The Authority proceeded from the State:
+      and therefore before introduction of Civill Government, the Gods of the
+      Heathen could not be Personated.
+    </p>
+<h3><a id="link2H_4_0188"></a>
+      The True God
+    </h3>
+<p>
+      The true God may be Personated. As he was; first, by Moses; who governed
+      the Israelites, (that were not his, but Gods people,) not in his own name,
+      with Hoc Dicit Moses; but in Gods Name, with Hoc Dicit Dominus. Secondly,
+      by the son of man, his own Son our Blessed Saviour Jesus Christ, that came
+      to reduce the Jewes, and induce all Nations into the Kingdome of his
+      Father; not as of himselfe, but as sent from his Father. And thirdly, by
+      the Holy Ghost, or Comforter, speaking, and working in the Apostles: which
+      Holy Ghost, was a Comforter that came not of himselfe; but was sent, and
+      proceeded from them both.
+    </p>
+<h3><a id="link2H_4_0189"></a>
+      A Multitude Of Men, How One Person
+    </h3>
+<p>
+      A Multitude of men, are made One Person, when they are by one man, or one
+      Person, Represented; so that it be done with the consent of every one of
+      that Multitude in particular. For it is the Unity of the Representer, not
+      the Unity of the Represented, that maketh the Person One. And it is the
+      Representer that beareth the Person, and but one Person: And Unity, cannot
+      otherwise be understood in Multitude.
+    </p>
+<h3><a id="link2H_4_0190"></a>
+      Every One Is Author
+    </h3>
+<p>
+      And because the Multitude naturally is not One, but Many; they cannot be
+      understood for one; but many Authors, of every thing their Representative
+      faith, or doth in their name; Every man giving their common Representer,
+      Authority from himselfe in particular; and owning all the actions the
+      Representer doth, in case they give him Authority without stint:
+      Otherwise, when they limit him in what, and how farre he shall represent
+      them, none of them owneth more, than they gave him commission to Act.
+    </p>
+<h3><a id="link2H_4_0191"></a>
+      An Actor May Be Many Men Made One By Plurality Of Voyces
+    </h3>
+<p>
+      And if the Representative consist of many men, the voyce of the greater
+      number, must be considered as the voyce of them all. For if the lesser
+      number pronounce (for example) in the Affirmative, and the greater in the
+      Negative, there will be Negatives more than enough to destroy the
+      Affirmatives; and thereby the excesse of Negatives, standing
+      uncontradicted, are the onely voyce the Representative hath.
+    </p>
+<h3><a id="link2H_4_0192"></a>
+      Representatives, When The Number Is Even, Unprofitable
+    </h3>
+<p>
+      And a Representative of even number, especially when the number is not
+      great, whereby the contradictory voyces are oftentimes equall, is
+      therefore oftentimes mute, and uncapable of Action. Yet in some cases
+      contradictory voyces equall in number, may determine a question; as in
+      condemning, or absolving, equality of votes, even in that they condemne
+      not, do absolve; but not on the contrary condemne, in that they absolve
+      not. For when a Cause is heard; not to condemne, is to absolve; but on the
+      contrary, to say that not absolving, is condemning, is not true. The like
+      it is in a deliberation of executing presently, or deferring till another
+      time; For when the voyces are equall, the not decreeing Execution, is a
+      decree of Dilation.
+    </p>
+<h3><a id="link2H_4_0193"></a>
+      Negative Voyce
+    </h3>
+<p>
+      Or if the number be odde, as three, or more, (men, or assemblies;) whereof
+      every one has by a Negative Voice, authority to take away the effect of
+      all the Affirmative Voices of the rest, This number is no Representative;
+      because by the diversity of Opinions, and Interests of men, it becomes
+      oftentimes, and in cases of the greatest consequence, a mute Person, and
+      unapt, as for may things else, so for the government of a Multitude,
+      especially in time of Warre.
+    </p>
+<p>
+      Of Authors there be two sorts. The first simply so called; which I have
+      before defined to be him, that owneth the Action of another simply. The
+      second is he, that owneth an Action, or Covenant of another conditionally;
+      that is to say, he undertaketh to do it, if the other doth it not, at, or
+      before a certain time. And these Authors conditionall, are generally
+      called SURETYES, in Latine Fidejussores, and Sponsores; and particularly
+      for Debt, Praedes; and for Appearance before a Judge, or Magistrate,
+      Vades.
+    </p>
+</div>)}
