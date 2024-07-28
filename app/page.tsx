af8769f6e9f6773ff825133940119b285224010b
Jasper Wong <wlcjasper@gmail.com>
2024-07-28 22:54:34 +0100

    Add

--- app/page.tsx
+++ app/page.tsx
@@ -9,90 +9,209 @@ export default function Page() {
       <h1 className="text-4xl font-bold mb-8">Thomas Hobbes's Leviathan</h1>
       <h2 className="text-4xl font-bold mb-8">Table of Contents</h2>
       <ol className="list-decimal list-inside space-y-6">
-        <li> OF SENSE</li>
-        <li> OF IMAGINATION</li>
-        <li> OF THE CONSEQUENCE OR TRAYNE OF IMAGINATIONS</li>
-        <li> OF SPEECH</li>
-        <li> OF REASON, AND SCIENCE.</li>
         <li>
-          OF THE INTERIOUR BEGINNINGS OF VOLUNTARY MOTIONS COMMONLY CALLED THE
-          PASSIONS, AND THE SPEECHES BY WHICH THEY ARE EXPRESSED.
-        </li>
-        <li> OF THE ENDS OR RESOLUTIONS OF DISCOURSE</li>
-        <li>
-          OF THE VERTUES COMMONLY CALLED INTELLECTUAL, AND THEIR CONTRARY
-          DEFECTS
-        </li>
-        <li> OF THE SEVERALL SUBJECTS OF KNOWLEDGE</li>
-        <li> OF POWER, WORTH, DIGNITY, HONOUR AND WORTHINESS</li>
-        <li> OF THE DIFFERENCE OF MANNERS</li>
-        <li> OF RELIGION</li>
-        <li>
-          OF THE NATURALL CONDITION OF MANKIND, AS CONCERNING THEIR FELICITY,
-          AND MISERY
-        </li>
-        <li>OF THE FIRST AND SECOND NATURALL LAWES, AND OF CONTRACTS</li>
-        <li> OF OTHER LAWES OF NATURE</li>
-        <li> OF PERSONS, AUTHORS, AND THINGS PERSONATED</li>
-        <li>OF THE CAUSES, GENERATION, AND DEFINITION OF A COMMON-WEALTH</li>
-        <li>. OF THE RIGHTS OF SOVERAIGNES BY INSTITUTION</li>
-        <li>
-          OF THE SEVERALL KINDS OF COMMON-WEALTH BY INSTITUTION, AND OF
-          SUCCESSION TO THE SOVERAIGNE POWER
-        </li>
-        <li> OF DOMINION PATERNALL AND DESPOTICALL</li>
-        <li> OF THE LIBERTY OF SUBJECTS</li>
-        <li> OF SYSTEMES SUBJECT, POLITICALL, AND PRIVATE</li>
-        <li>. OF THE PUBLIQUE MINISTERS OF SOVERAIGN POWER</li>
-        <li>OF THE NUTRITION, AND PROCREATION OF A COMMON-WEALTH</li>
-        <li> OF COUNSELL</li>
-        <li> OF CIVILL LAWES</li>
-        <li>. OF CRIMES, EXCUSES, AND EXTENUATIONS</li>
-        <li>I. OF PUNISHMENTS, AND REWARDS</li>
-        <li>
-          OF THOSE THINGS THAT WEAKEN, OR TEND TO THE DISSOLUTION OF A
-          COMMON-WEALTH
-        </li>
-        <li> OF THE OFFICE OF THE SOVERAIGN REPRESENTATIVE</li>
-        <li> OF THE KINGDOME OF GOD BY NATURE</li>
-        <li>. OF THE PRINCIPLES OF CHRISTIAN POLITIQUES</li>
-        <li>
-          I. OF THE NUMBER, ANTIQUITY, SCOPE, AUTHORITY, AND INTERPRETERS OF THE
-          BOOKS OF HOLY SCRIPTURE
+          <a href="/chapter/1"> OF SENSE</a>
         </li>
         <li>
-          . OF THE SIGNIFICATION OF SPIRIT, ANGEL, AND INSPIRATION IN THE BOOKS
-          OF HOLY SCRIPTURE
+          <a href="/chapter/2"> OF IMAGINATION</a>
         </li>
         <li>
-          OF THE SIGNIFICATION IN SCRIPTURE OF KINGDOME OF GOD, OF HOLY, SACRED,
-          AND SACRAMENT
+          <a href="/chapter/3"> OF THE CONSEQUENCE OR TRAYNE OF IMAGINATIONS</a>
         </li>
-        <li>. OF THE WORD OF GOD, AND OF PROPHETS</li>
-        <li>I. OF MIRACLES, AND THEIR USE</li>
         <li>
-          II. OF THE SIGNIFICATION IN SCRIPTURE OF ETERNALL LIFE, HELL,
-          SALVATION, THE WORLD TO COME, AND REDEMPTION
+          <a href="/chapter/4"> OF SPEECH</a>
         </li>
-        <li>. OF THE SIGNIFICATION IN SCRIPTURE OF THE WORD CHURCH</li>
         <li>
-          OF THE RIGHTS OF THE KINGDOME OF GOD, IN ABRAHAM, MOSES, HIGH PRIESTS,
-          AND THE KINGS OF JUDAH
+          <a href="/chapter/5"> OF REASON, AND SCIENCE.</a>
         </li>
-        <li> OF THE OFFICE OF OUR BLESSED SAVIOUR</li>
-        <li> OF POWER ECCLESIASTICALL</li>
         <li>
-          . OF WHAT IS NECESSARY FOR A MANS RECEPTION INTO THE KINGDOME OF
-          HEAVEN
+          <a href="/chapter/6">
+            OF THE INTERIOUR BEGINNINGS OF VOLUNTARY MOTIONS COMMONLY CALLED
+            THEPASSIONS, AND THE SPEECHES BY WHICH THEY ARE EXPRESSED.
+          </a>
         </li>
-        <li>OF SPIRITUALL DARKNESSE FROM MISINTERPRETATION OF SCRIPTURE</li>
         <li>
-          OF DAEMONOLOGY, AND OTHER RELIQUES OF THE RELIGION OF THE GENTILES
+          <a href="/chapter/7"> OF THE ENDS OR RESOLUTIONS OF DISCOURSE</a>
         </li>
-        <li>OF DARKNESSE FROM VAIN PHILOSOPHY, AND FABULOUS TRADITIONS</li>
         <li>
-          OF THE BENEFIT THAT PROCEEDETH FROM SUCH DARKNESSE, AND TO WHOM IT
-          ACCREWETH
+          <a href="/chapter/8">
+            OF THE VERTUES COMMONLY CALLED INTELLECTUAL, AND THEIR
+            CONTRARYDEFECTS
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/9"> OF THE SEVERALL SUBJECTS OF KNOWLEDGE</a>
+        </li>
+        <li>
+          <a href="/chapter/10">
+            {" "}
+            OF POWER, WORTH, DIGNITY, HONOUR AND WORTHINESS
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/11"> OF THE DIFFERENCE OF MANNERS</a>
+        </li>
+        <li>
+          <a href="/chapter/12"> OF RELIGION</a>
+        </li>
+        <li>
+          <a href="/chapter/13">
+            OF THE NATURALL CONDITION OF MANKIND, AS CONCERNING THEIR
+            FELICITY,AND MISERY
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/14">
+            OF THE FIRST AND SECOND NATURALL LAWES, AND OF CONTRACTS
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/15"> OF OTHER LAWES OF NATURE</a>
+        </li>
+        <li>
+          <a href="/chapter/16"> OF PERSONS, AUTHORS, AND THINGS PERSONATED</a>
+        </li>
+        <li>
+          <a href="/chapter/17">
+            OF THE CAUSES, GENERATION, AND DEFINITION OF A COMMON-WEALTH
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/18">
+            . OF THE RIGHTS OF SOVERAIGNES BY INSTITUTION
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/19">
+            OF THE SEVERALL KINDS OF COMMON-WEALTH BY INSTITUTION, AND
+            OFSUCCESSION TO THE SOVERAIGNE POWER
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/20"> OF DOMINION PATERNALL AND DESPOTICALL</a>
+        </li>
+        <li>
+          <a href="/chapter/21"> OF THE LIBERTY OF SUBJECTS</a>
+        </li>
+        <li>
+          <a href="/chapter/22">
+            {" "}
+            OF SYSTEMES SUBJECT, POLITICALL, AND PRIVATE
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/23">
+            . OF THE PUBLIQUE MINISTERS OF SOVERAIGN POWER
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/24">
+            OF THE NUTRITION, AND PROCREATION OF A COMMON-WEALTH
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/25"> OF COUNSELL</a>
+        </li>
+        <li>
+          <a href="/chapter/26"> OF CIVILL LAWES</a>
+        </li>
+        <li>
+          <a href="/chapter/27">. OF CRIMES, EXCUSES, AND EXTENUATIONS</a>
+        </li>
+        <li>
+          <a href="/chapter/28">I. OF PUNISHMENTS, AND REWARDS</a>
+        </li>
+        <li>
+          <a href="/chapter/29">
+            OF THOSE THINGS THAT WEAKEN, OR TEND TO THE DISSOLUTION OF
+            ACOMMON-WEALTH
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/30">
+            {" "}
+            OF THE OFFICE OF THE SOVERAIGN REPRESENTATIVE
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/31"> OF THE KINGDOME OF GOD BY NATURE</a>
+        </li>
+        <li>
+          <a href="/chapter/32">. OF THE PRINCIPLES OF CHRISTIAN POLITIQUES</a>
+        </li>
+        <li>
+          <a href="/chapter/33">
+            I. OF THE NUMBER, ANTIQUITY, SCOPE, AUTHORITY, AND INTERPRETERS OF
+            THEBOOKS OF HOLY SCRIPTURE
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/34">
+            . OF THE SIGNIFICATION OF SPIRIT, ANGEL, AND INSPIRATION IN THE
+            BOOKSOF HOLY SCRIPTURE
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/35">
+            OF THE SIGNIFICATION IN SCRIPTURE OF KINGDOME OF GOD, OF HOLY,
+            SACRED,AND SACRAMENT
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/36">. OF THE WORD OF GOD, AND OF PROPHETS</a>
+        </li>
+        <li>
+          <a href="/chapter/37">I. OF MIRACLES, AND THEIR USE</a>
+        </li>
+        <li>
+          <a href="/chapter/38">
+            II. OF THE SIGNIFICATION IN SCRIPTURE OF ETERNALL LIFE,
+            HELL,SALVATION, THE WORLD TO COME, AND REDEMPTION
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/39">
+            . OF THE SIGNIFICATION IN SCRIPTURE OF THE WORD CHURCH
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/40">
+            OF THE RIGHTS OF THE KINGDOME OF GOD, IN ABRAHAM, MOSES, HIGH
+            PRIESTS,AND THE KINGS OF JUDAH
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/41"> OF THE OFFICE OF OUR BLESSED SAVIOUR</a>
+        </li>
+        <li>
+          <a href="/chapter/42"> OF POWER ECCLESIASTICALL</a>
+        </li>
+        <li>
+          <a href="/chapter/43">
+            . OF WHAT IS NECESSARY FOR A MANS RECEPTION INTO THE KINGDOME
+            OFHEAVEN
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/44">
+            OF SPIRITUALL DARKNESSE FROM MISINTERPRETATION OF SCRIPTURE
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/45">
+            OF DAEMONOLOGY, AND OTHER RELIQUES OF THE RELIGION OF THE GENTILES
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/46">
+            OF DARKNESSE FROM VAIN PHILOSOPHY, AND FABULOUS TRADITIONS
+          </a>
+        </li>
+        <li>
+          <a href="/chapter/47">
+            OF THE BENEFIT THAT PROCEEDETH FROM SUCH DARKNESSE, AND TO WHOM
+            ITACCREWETH
+          </a>
         </li>
       </ol>
     </div>
