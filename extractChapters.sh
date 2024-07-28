8a3320d02b10915e96ce70f9b62b479642241491
Jasper Wong <wlcjasper@gmail.com>
2024-07-28 21:13:29 +0100

    Simplify for better understanding

--- extractChapters.sh
+++ extractChapters.sh
@@ -12,32 +12,17 @@ echo "Starting to process file: $input_file"
 sed -n '/<div class="chapter">/,/<!--end chapter-->/p' "$input_file" | 
 while IFS= read -r line
 do
-    
     if [[ $line == '<div class="chapter">'* ]]; then
         # Start of a new chapter, create a new file
         output_file="chapter_$counter.txt"
     elif [[ $line == *'<!--end chapter-->' ]]; then
-        # End of the chapter, increment counter
-        echo "Current counter: $counter"
+        # End of the chapter, append the end tag and increment counter
+        echo "$line" >> "$output_file"
         ((counter++))
         echo "Incremented counter to: $counter"
     else
-        # Content of the chapter, append to current file
-        echo "Appending content to $output_file"
         echo "$line" >> "$output_file"
     fi
 done
-
-echo "Finished processing all lines"
-
-# Read the final counter value
-if [ -f counter.txt ]; then
-    final_counter=$(<counter.txt)
-    echo "Total chapters extracted: $final_counter"
-    rm counter.txt
-    echo "Removed temporary counter file"
-else
-    echo "No chapters were extracted"
-fi
 
 echo "Script execution completed"
