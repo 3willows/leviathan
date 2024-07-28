09343791f699741fed0258c1268775fca5269f50
Jasper Wong <wlcjasper@gmail.com>
2024-07-28 21:34:54 +0100

    Output goes to a directory

--- extractChapters.sh
+++ extractChapters.sh
@@ -3,6 +3,12 @@
 # Read the input file
 input_file="leviathan.html"
 
+# Output directory
+output_dir="chapters"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
 # Counter for naming output files
 counter=0
 
@@ -16,7 +22,7 @@ do
         ((counter++))
         echo "Incremented counter to: $counter"      
         # Start of a new chapter, create a new file
-        output_file="chapter_$counter.txt"
+        output_file="$output_dir/chapter_$counter.html"
     elif [[ $line == *'<!--end chapter-->' ]]; then
         # End of the chapter, append the end tag and increment counter
         echo "$line" >> "$output_file"
