55113cd1b10c4ff2db6028b4b5e8d1f7cdd8cfe9
Jasper Wong <wlcjasper@gmail.com>
2024-07-29 01:34:10 +0100

    Relative path with useRouter, usePathname and Link.

--- app/page.tsx
+++ app/page.tsx
@@ -1,215 +1,265 @@
+"use client"
+
 import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { usePathname } from "next/navigation"
 
 export default function Page() {
+  const router = useRouter()
+  const pathname = usePathname()
+
   return (
     <div className="bg-[#f5f5f5] text-[#333] font-serif mx-auto max-w-4xl px-8 py-12 shadow-lg">
-      <h1 className="text-4xl font-bold mb-8">Thomas Hobbes &rsquo;s Leviathan</h1>
+      <h1 className="text-4xl font-bold mb-8">
+        Thomas Hobbes &rsquo;s Leviathan
+      </h1>
       <h2 className="text-4xl font-bold mb-8">Table of Contents</h2>
       <ol className="list-decimal list-inside space-y-6">
         <li>
-        <base href="./users/" />
-          <Link href="/chapter/1"> OF SENSE</Link>
+          <Link href={`${pathname}/chapter/1`}> OF SENSE</Link>
         </li>
         <li>
-          <a href="/chapter/2"> OF IMAGINATION</a>
+          <Link href={`${pathname}/chapter/2`}> OF IMAGINATION</Link>
         </li>
         <li>
-          <a href="/chapter/3"> OF THE CONSEQUENCE OR TRAYNE OF IMAGINATIONS</a>
+          <Link href={`${pathname}/chapter/3`}>
+            {" "}
+            OF THE CONSEQUENCE OR TRAYNE OF IMAGINATIONS
+          </Link>
         </li>
         <li>
-          <a href="/chapter/4"> OF SPEECH</a>
+          <Link href={`${pathname}/chapter/4`}> OF SPEECH</Link>
         </li>
         <li>
-          <a href="/chapter/5"> OF REASON, AND SCIENCE.</a>
+          <Link href={`${pathname}/chapter/5`}> OF REASON, AND SCIENCE.</Link>
         </li>
         <li>
-          <a href="/chapter/6">
+          <Link href={`${pathname}/chapter/6`}>
             OF THE INTERIOUR BEGINNINGS OF VOLUNTARY MOTIONS COMMONLY CALLED
             THEPASSIONS, AND THE SPEECHES BY WHICH THEY ARE EXPRESSED.
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/7"> OF THE ENDS OR RESOLUTIONS OF DISCOURSE</a>
+          <Link href={`${pathname}/chapter/7`}>
+            {" "}
+            OF THE ENDS OR RESOLUTIONS OF DISCOURSE
+          </Link>
         </li>
         <li>
-          <a href="/chapter/8">
+          <Link href={`${pathname}/chapter/8`}>
             OF THE VERTUES COMMONLY CALLED INTELLECTUAL, AND THEIR
             CONTRARYDEFECTS
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/9"> OF THE SEVERALL SUBJECTS OF KNOWLEDGE</a>
+          <Link href={`${pathname}/chapter/9`}>
+            {" "}
+            OF THE SEVERALL SUBJECTS OF KNOWLEDGE
+          </Link>
         </li>
         <li>
-          <a href="/chapter/10">
+          <Link href={`${pathname}/chapter/10`}>
             {" "}
             OF POWER, WORTH, DIGNITY, HONOUR AND WORTHINESS
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/11"> OF THE DIFFERENCE OF MANNERS</a>
+          <Link href={`${pathname}/chapter/1`}>
+            {" "}
+            OF THE DIFFERENCE OF MANNERS
+          </Link>
         </li>
         <li>
-          <a href="/chapter/12"> OF RELIGION</a>
+          <Link href={`${pathname}/chapter/2`}> OF RELIGION</Link>
         </li>
         <li>
-          <a href="/chapter/13">
+          <Link href={`${pathname}/chapter/13`}>
             OF THE NATURALL CONDITION OF MANKIND, AS CONCERNING THEIR
             FELICITY,AND MISERY
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/14">
+          <Link href={`${pathname}/chapter/14`}>
             OF THE FIRST AND SECOND NATURALL LAWES, AND OF CONTRACTS
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/15"> OF OTHER LAWES OF NATURE</a>
+          <Link href={`${pathname}/chapter/5`}> OF OTHER LAWES OF NATURE</Link>
         </li>
         <li>
-          <a href="/chapter/16"> OF PERSONS, AUTHORS, AND THINGS PERSONATED</a>
+          <Link href={`${pathname}/chapter/6`}>
+            {" "}
+            OF PERSONS, AUTHORS, AND THINGS PERSONATED
+          </Link>
         </li>
         <li>
-          <a href="/chapter/17">
+          <Link href={`${pathname}/chapter/17`}>
             OF THE CAUSES, GENERATION, AND DEFINITION OF A COMMON-WEALTH
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/18">
+          <Link href={`${pathname}/chapter/18`}>
             . OF THE RIGHTS OF SOVERAIGNES BY INSTITUTION
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/19">
+          <Link href={`${pathname}/chapter/19`}>
             OF THE SEVERALL KINDS OF COMMON-WEALTH BY INSTITUTION, AND
             OFSUCCESSION TO THE SOVERAIGNE POWER
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/20"> OF DOMINION PATERNALL AND DESPOTICALL</a>
+          <Link href={`${pathname}/chapter/0`}>
+            {" "}
+            OF DOMINION PATERNALL AND DESPOTICALL
+          </Link>
         </li>
         <li>
-          <a href="/chapter/21"> OF THE LIBERTY OF SUBJECTS</a>
+          <Link href={`${pathname}/chapter/1`}>
+            {" "}
+            OF THE LIBERTY OF SUBJECTS
+          </Link>
         </li>
         <li>
-          <a href="/chapter/22">
+          <Link href={`${pathname}/chapter/22`}>
             {" "}
             OF SYSTEMES SUBJECT, POLITICALL, AND PRIVATE
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/23">
+          <Link href={`${pathname}/chapter/23`}>
             . OF THE PUBLIQUE MINISTERS OF SOVERAIGN POWER
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/24">
+          <Link href={`${pathname}/chapter/24`}>
             OF THE NUTRITION, AND PROCREATION OF A COMMON-WEALTH
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/25"> OF COUNSELL</a>
+          <Link href={`${pathname}/chapter/5`}> OF COUNSELL</Link>
         </li>
         <li>
-          <a href="/chapter/26"> OF CIVILL LAWES</a>
+          <Link href={`${pathname}/chapter/6`}> OF CIVILL LAWES</Link>
         </li>
         <li>
-          <a href="/chapter/27">. OF CRIMES, EXCUSES, AND EXTENUATIONS</a>
+          <Link href={`${pathname}/chapter/7`}>
+            {" "}
+            . OF CRIMES, EXCUSES, AND EXTENUATIONS
+          </Link>
         </li>
         <li>
-          <a href="/chapter/28">I. OF PUNISHMENTS, AND REWARDS</a>
+          <Link href={`${pathname}/chapter/8`}>
+            {" "}
+            I. OF PUNISHMENTS, AND REWARDS
+          </Link>
         </li>
         <li>
-          <a href="/chapter/29">
+          <Link href={`${pathname}/chapter/29`}>
             OF THOSE THINGS THAT WEAKEN, OR TEND TO THE DISSOLUTION OF
             ACOMMON-WEALTH
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/30">
+          <Link href={`${pathname}/chapter/30`}>
             {" "}
             OF THE OFFICE OF THE SOVERAIGN REPRESENTATIVE
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/31"> OF THE KINGDOME OF GOD BY NATURE</a>
+          <Link href={`${pathname}/chapter/1`}>
+            {" "}
+            OF THE KINGDOME OF GOD BY NATURE
+          </Link>
         </li>
         <li>
-          <a href="/chapter/32">. OF THE PRINCIPLES OF CHRISTIAN POLITIQUES</a>
+          <Link href={`${pathname}/chapter/2`}>
+            {" "}
+            . OF THE PRINCIPLES OF CHRISTIAN POLITIQUES
+          </Link>
         </li>
         <li>
-          <a href="/chapter/33">
+          <Link href={`${pathname}/chapter/33`}>
             I. OF THE NUMBER, ANTIQUITY, SCOPE, AUTHORITY, AND INTERPRETERS OF
             THEBOOKS OF HOLY SCRIPTURE
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/34">
+          <Link href={`${pathname}/chapter/34`}>
             . OF THE SIGNIFICATION OF SPIRIT, ANGEL, AND INSPIRATION IN THE
             BOOKSOF HOLY SCRIPTURE
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/35">
+          <Link href={`${pathname}/chapter/35`}>
             OF THE SIGNIFICATION IN SCRIPTURE OF KINGDOME OF GOD, OF HOLY,
             SACRED,AND SACRAMENT
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/36">. OF THE WORD OF GOD, AND OF PROPHETS</a>
+          <Link href={`${pathname}/chapter/6`}>
+            {" "}
+            . OF THE WORD OF GOD, AND OF PROPHETS
+          </Link>
         </li>
         <li>
-          <a href="/chapter/37">I. OF MIRACLES, AND THEIR USE</a>
+          <Link href={`${pathname}/chapter/7`}>
+            {" "}
+            I. OF MIRACLES, AND THEIR USE
+          </Link>
         </li>
         <li>
-          <a href="/chapter/38">
+          <Link href={`${pathname}/chapter/38`}>
             II. OF THE SIGNIFICATION IN SCRIPTURE OF ETERNALL LIFE,
             HELL,SALVATION, THE WORLD TO COME, AND REDEMPTION
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/39">
+          <Link href={`${pathname}/chapter/39`}>
             . OF THE SIGNIFICATION IN SCRIPTURE OF THE WORD CHURCH
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/40">
+          <Link href={`${pathname}/chapter/40`}>
             OF THE RIGHTS OF THE KINGDOME OF GOD, IN ABRAHAM, MOSES, HIGH
             PRIESTS,AND THE KINGS OF JUDAH
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/41"> OF THE OFFICE OF OUR BLESSED SAVIOUR</a>
+          <Link href={`${pathname}/chapter/1`}>
+            {" "}
+            OF THE OFFICE OF OUR BLESSED SAVIOUR
+          </Link>
         </li>
         <li>
-          <a href="/chapter/42"> OF POWER ECCLESIASTICALL</a>
+          <Link href={`${pathname}/chapter/2`}> OF POWER ECCLESIASTICALL</Link>
         </li>
         <li>
-          <a href="/chapter/43">
+          <Link href={`${pathname}/chapter/43`}>
             . OF WHAT IS NECESSARY FOR A MANS RECEPTION INTO THE KINGDOME
             OFHEAVEN
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/44">
+          <Link href={`${pathname}/chapter/44`}>
             OF SPIRITUALL DARKNESSE FROM MISINTERPRETATION OF SCRIPTURE
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/45">
+          <Link href={`${pathname}/chapter/45`}>
             OF DAEMONOLOGY, AND OTHER RELIQUES OF THE RELIGION OF THE GENTILES
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/46">
+          <Link href={`${pathname}/chapter/46`}>
             OF DARKNESSE FROM VAIN PHILOSOPHY, AND FABULOUS TRADITIONS
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/chapter/47">
+          <Link href={`${pathname}/chapter/47`}>
             OF THE BENEFIT THAT PROCEEDETH FROM SUCH DARKNESSE, AND TO WHOM
             ITACCREWETH
-          </a>
+          </Link>
         </li>
       </ol>
     </div>
