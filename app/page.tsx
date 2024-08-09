610045b7dd7d66b106b4803a9edbc0fb58cb73f0
Jasper Wong <wlcjasper@gmail.com>
2024-08-09 15:39:50 +0100

    Add link

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
       <ul className="list-decimal list-inside space-y-6">
         <li className="text-red-600">
           <Link href={`/hobbes`}> The book </Link></li>
-          <li className="text-yellow-600">
-         Motivation (to be written up) </li>
+          <li className="text-yellow-600"> <a href="https://3willows.github.io/2024-07-29-learn-next.js-leviathan/" rel="noopener noreferrer" >Motivation</a>
+           </li>
          <li className="text-green-600"><a href="https://github.com/3willows/leviathan">Public repo</a></li>
       </ul>
     </div>
